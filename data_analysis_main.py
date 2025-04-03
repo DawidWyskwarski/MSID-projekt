b2b2eec5693981d78f6a9b0cf7c67788c0d5ec34
DawidWyskwarski <153649273+DawidWyskwarski@users.noreply.github.com>
2025-04-03 15:00:02 +0200

    error and histograms

--- data_analysis_main.py
+++ data_analysis_main.py
@@ -26,6 +26,7 @@ def box_plots(ys:pd.DataFrame ,x, data_frame):
     for y in ys:
         plt.figure(figsize=(16, 5))
         sns.boxplot(x=x, y=y, data=data_frame)
+        plt.title(f"{y} x {x}")
 
         plt.savefig(f"plots/boxplots/{y} x {x}.png")
 
@@ -35,8 +36,29 @@ def violin_plots(ys:pd.DataFrame ,x, data_frame):
     for y in ys:
         plt.figure(figsize=(16, 5))
         sns.violinplot(x=x, y=y, data=data_frame)
+        plt.title(f"{y} x {x}")
 
         plt.savefig(f"plots/violinplots/{y} x {x}.png")
+
+        plt.show()
+
+def error_bars(numerical:pd.DataFrame ,x, data_frame, eb_type):
+    for col in numerical:
+        plt.figure(figsize=(12, 4))
+        plt.title(f"{eb_type} {col} x {x}")
+
+        sns.pointplot(x='NObeyesdad', y=col, data=data_frame, errorbar=eb_type, linestyles='none')
+
+        plt.savefig(f"plots/error_bars/{eb_type} {col} x {x}.png")
+
+        plt.show()
+
+def histograms(numerical: pd.DataFrame, data_frame=None, hue=None):
+    for col in numerical:
+        sns.histplot(data=data_frame if data_frame is not None else numerical, x=col,hue=hue)
+
+        plt.title(f"{col} {f"with {hue} " if hue else ""}histogram")
+        plt.savefig(f"plots/histograms/{col} {f"with {hue} " if hue else ""}histogram.png")
 
         plt.show()
 
@@ -45,6 +67,7 @@ def correlation_heatmap(df,name):
 
     plt.savefig(f"plots/heatmap/{name} correlation.png")
     plt.show()
+
 
 def analysis(file_name):
     df = pd.read_csv(file_name)
@@ -63,6 +86,17 @@ def analysis(file_name):
     box_plots(numerical,"NObeyesdad",df)
     violin_plots(numerical,"NObeyesdad",df)
 
+    #save error bar plots of the numerical x NObeysesdad
+
+    error_bars(numerical,"NObeyesdad",df, "sd")
+    error_bars(numerical,"NObeyesdad",df, "se")
+
+    #save numerical histograms
+    histograms(numerical)
+
+    #save parametric histograms
+    histograms(numerical=numerical,data_frame=df,hue='Gender')
+    histograms(numerical=numerical,data_frame=df,hue='NObeyesdad')
 
     #save the correlation heatmap
     correlation_heatmap(numerical,"obesity")
