a446c705d615b8735311d23b9f765ffc16330e56
DawidWyskwarski <153649273+DawidWyskwarski@users.noreply.github.com>
2025-05-21 15:02:53 +0200

    Changed the dataset for good

--- data_analysis_main.py
+++ data_analysis_main.py
@@ -1,12 +1,23 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
 from pathlib import Path
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
+from sklearn.linear_model import BayesianRidge
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+from utils import remove_nans, format_df
+
+TARGET = 'music_genre'
 
 def summarize_numerical(numerical):
     num_summary: pd.DataFrame = numerical.describe(percentiles=[0.05, 0.95]).loc[
-        ['mean', '50%', 'min', 'max', 'std', '5%', '95%']]
+        ['mean', '50%', 'min', 'max', 'std', '5%', '95%']
+    ]
     num_summary.loc['missing'] = numerical.isna().sum()
+    num_summary = num_summary.reset_index().rename(columns={'index': 'statistic'})
 
     return num_summary
 
@@ -16,10 +27,15 @@ def summarize_categorical(categorical):
 
     cat_summary.loc['unique classes'] = categorical.nunique()
 
-    for col in categorical.columns:
-        proportions = categorical[col].value_counts(normalize=True)
-        for idx, value in proportions.items():
-            cat_summary.loc[f'{idx}_proportion', col] = value
+    cat_summary = cat_summary.reset_index().rename(columns={'index': 'statistic'})
+
+    #For the love of God don't uncomment it
+    #Too many categories :O
+
+    #for col in categorical.columns:
+    #    proportions = categorical[col].value_counts(normalize=True)
+    #    for idx, value in proportions.items():
+    #        cat_summary.loc[f'{idx}_proportion', col] = value
 
     return cat_summary
 
@@ -48,23 +64,51 @@ def error_bars(numerical:pd.DataFrame ,x, data_frame, eb_type):
         plt.figure(figsize=(12, 4))
         plt.title(f"{eb_type} {col} x {x}")
 
-        sns.pointplot(x='NObeyesdad', y=col, data=data_frame, errorbar=eb_type, linestyles='none')
+        sns.pointplot(x=x, y=col, data=data_frame, errorbar=eb_type, linestyles='none')
 
         plt.savefig(f"plots/error_bars/{eb_type} {col} x {x}.png")
 
         plt.show()
 
-def histograms(numerical: pd.DataFrame, data_frame=None, hue=None):
-    for col in numerical:
-        sns.histplot(data=data_frame if data_frame is not None else numerical, x=col,hue=hue)
+def histograms(df: pd.DataFrame):
+    df.hist(figsize=(12, 12), bins=25)
+    plt.savefig(f"plots/histograms/numerical_histograms")
 
-        plt.title(f"{col} {f"with {hue} " if hue else ""}histogram")
-        plt.savefig(f"plots/histograms/{col} {f"with {hue} " if hue else ""}histogram.png")
+    plt.show()
+
+    genres = df['music_genre'].unique()
+
+    mode_palette = {
+        'Minor': 'blue',
+        'Major': 'orange'
+    }
+
+    keys_order = df['key'].unique()
+    keys_order.sort()
+
+    for genre in genres:
+        subset = df[df['music_genre'] == genre]
+
+        plt.figure(figsize=(12, 4))
+        sns.countplot(data=subset, x='key', hue='mode', palette=mode_palette, order=keys_order)
+        plt.title(genre)
+
+        plt.savefig(f"plots/histograms/{genre} keys x modes")
 
         plt.show()
 
+
+    plt.figure(figsize=(12, 4))
+    sns.countplot(data=df, x='key', hue='mode', palette=mode_palette, order=keys_order)
+    plt.title("Keys-mode hist")
+
+    plt.savefig(f"plots/histograms/all keys x modes")
+
+    plt.show()
+
 def correlation_heatmap(df,name):
-    sns.heatmap(df.corr(), annot=True, cmap="Greens")
+    plt.figure(figsize=(15, 15))
+    sns.heatmap(df.corr(), annot=True, cmap="Greens", fmt=".2f")
 
     plt.savefig(f"plots/heatmap/{name} correlation.png")
     plt.show()
@@ -87,37 +131,33 @@ def reg_lines(numerical: pd.DataFrame):
 def analysis(file_name):
     df = pd.read_csv(file_name)
 
-    #divide into numerical and categorical
+    remove_nans(df)
+
+    ### Divide into numerical and categorical
     numerical: pd.DataFrame = df.select_dtypes(exclude=object)
     categorical = df.select_dtypes(include=object)
 
-    #save the numerical and categorical summary to csv
-
+    ### Save the numerical and categorical summary to csv
     summarize_numerical(numerical).to_csv("summaries/numerical_summary.csv", index=False)
     summarize_categorical(categorical).to_csv("summaries/categorical_summary.csv", index=False)
 
-    #save the box plots of the numerical x NObeysesdad
+    ### Save the box plots of the numerical x music_genre
+    box_plots(numerical,TARGET,df)
+    violin_plots(numerical,TARGET,df)
 
-    box_plots(numerical,"NObeyesdad",df)
-    violin_plots(numerical,"NObeyesdad",df)
+    ### Save error bar plots of the numerical x music_genre
+    error_bars(numerical,TARGET,df, "sd")
+    error_bars(numerical,TARGET,df, "se")
 
-    #save error bar plots of the numerical x NObeysesdad
+    ### Save numerical histograms
+    histograms(df)
 
-    error_bars(numerical,"NObeyesdad",df, "sd")
-    error_bars(numerical,"NObeyesdad",df, "se")
+    df = format_df(df)
+    ### Save the correlation heatmap
+    correlation_heatmap(df.select_dtypes(exclude=object), TARGET)
 
-    #save numerical histograms
-    histograms(numerical)
-
-    #save parametric histograms
-    histograms(numerical=numerical,data_frame=df,hue='Gender')
-    histograms(numerical=numerical,data_frame=df,hue='NObeyesdad')
-
-    #save the correlation heatmap
-    correlation_heatmap(numerical,"obesity")
-
-    #save the regression lines for the features with correlation >= 0.2
-    reg_lines(numerical)
+    ### Save the regression lines for the features with correlation >= 0.2
+    reg_lines(df.select_dtypes(exclude=object))
 
 def create_dirs():
     directories = ["plots", "plots/boxplots", "plots/error_bars", "plots/heatmap", "plots/histograms", "plots/regline",
@@ -130,4 +170,4 @@ def create_dirs():
 if __name__ == "__main__":
 
     create_dirs()
-    analysis("../data/ObesityDataSet_raw_and_data_sinthetic.csv")
+    analysis("data/music_genre.csv")
---
+++ utils.py
@@ -0,0 +1,47 @@
+import numpy as np
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
+from sklearn.linear_model import BayesianRidge
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+import pandas as pd
+
+def remove_nans(df):
+    df['artist_name'] = df['artist_name'].replace('empty_field', np.nan)
+    df['tempo'] = df['tempo'].replace('?', np.nan)
+
+    df.dropna(inplace=True)
+
+    df['duration_ms'] = df['duration_ms'].replace(-1, np.nan)
+
+    imputer = IterativeImputer(estimator=BayesianRidge(), random_state=42)
+
+    df['duration_ms'] = imputer.fit_transform(df[['duration_ms']])
+
+def format_df(df):
+    #get rid of nans
+    remove_nans(df)
+
+    #Don't need it
+    df.drop('obtained_date', inplace=True, axis=1)
+    df.drop('instance_id', inplace=True, axis=1)
+
+    #It was a string smh
+    df['tempo'] = df['tempo'].apply(lambda x: float(x))
+
+    #Encoding
+    le_keys = LabelEncoder()
+    df['key'] = le_keys.fit_transform(df['key'])
+
+    mode_encoding = {
+        'Minor': 0,
+        'Major': 1
+    }
+    df['mode'] = df['mode'].map(mode_encoding)
+
+    ohe = OneHotEncoder(sparse_output=False).set_output(transform='pandas')
+    encoded_genres = ohe.fit_transform(df[['music_genre']])
+
+    df = pd.concat([df, encoded_genres], axis=1).drop(columns=['music_genre'])
+
+    return df
